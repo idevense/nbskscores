88a8b9575ecf3e53c664162f1e4096e5b95e2ded
Unknown <idevense@gmail.com>
2017-11-10 15:32:58 +0100

    fixed jitter in searchform

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form role="search" method="get" class="searchform group" action="<?php echo home_url( '/' ); ?>"> 
+<i class="fa fa-search" type="icon"></i>
 <label>
 <input type="search" class="search-field"
 placeholder="<?php echo esc_attr_x( 'Søk', 'placeholder' ) ?>"
@@ -7,5 +8,5 @@ onblur="this.placeholder = '<?php echo esc_attr_x( 'Søk', 'placeholder' ) ?>'"
 value="<?php echo get_search_query() ?>" name="s"
 title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 </label>
-    <i class="fa fa-search fa-2" type="icon"></i>
+    
 </form>
